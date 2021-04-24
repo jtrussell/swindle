b5ba0c6a73d8a792f0178b62ae869f13c6a70ef0
Justin Russell <justin.russell@clublocker.com>
2021-04-24 16:20:09 -0400

    Add sign up flow

--- src/registration/models.py
+++ src/registration/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
-    discord_handle = models.CharField(max_length=100, unique=True)
+    discord_handle = models.CharField(max_length=100, unique=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('profile')
 
 
 @receiver(post_save, sender=User)
--- src/registration/views.py
+++ src/registration/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import NewUserForm, UpdateUserForm, UserProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
@@ -28,6 +31,22 @@ def profile(request):
 def sign_up(request):
     user_form = NewUserForm()
     profile_form = UserProfileForm()
+    if request.method == 'POST':
+        user_form = NewUserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = UserProfile.objects.get(user=user.id)
+            print(profile_form.cleaned_data)
+            discord_handle = profile_form.cleaned_data['discord_handle']
+            profile.discord_handle = discord_handle
+            profile.save()
+            login(request, user)
+            return redirect(profile)
+    else:
+        user_form = NewUserForm()
+        profile_form = UserProfileForm()
+
     return render(request, 'registration/sign-up.html', {
         'user_form': user_form,
         'profile_form': profile_form,
