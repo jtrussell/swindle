efebf36be051851ec38bf55a3cb7b77e45048346
jtrussell <jus.russell@gmail.com>
2021-05-02 22:01:12 -0400

    Get DB connection from environment

--- src/swindle/settings.py
+++ src/swindle/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'swindle.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(default='sqlite:///{}'.format(BASE_DIR / 'db.sqlite3')),
 }
 
 
